5fb9c8ae95662a1aaae770769b04a43e3c315654
bohao zhang <eeybz1@nottingham.ac.uk>
2026-02-13 10:04:44 +0800

    WS5 Exercise1 - Added Doxygen configuration and documentation

---
+++ Worksheet5/adder.h
@@ -0,0 +1,20 @@
+#ifndef MATHSLIB_ADDER_H
+#define MATHSLIB_ADDER_H
+
+/**
+ * @file adder.h
+ * @brief Declarations of exported maths library functions
+ */
+
+ /**
+  * @brief Add two integers
+  *
+  * This function takes two integers and returns their sum.
+  *
+  * @param a First integer
+  * @param b Second integer
+  * @return Sum of a and b
+  */
+int add(int a, int b);
+
+#endif
